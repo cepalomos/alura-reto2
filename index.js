8908e8c42404eddbcdd0e2ccfbf0b201559b1381
Carlos Palomo Serna <cepalomos@outlook.es>
2022-04-03 21:29:41 -0500

    funcionalidad completada parcialmente

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-const btnIngresar =document.getElementById("ingresar");
+// const textUsadas = document.getElementById("letraUsadas");
+const finalizacion = document.getElementById("finalizacion");
+const contenedorCanvas = document.getElementById("tutorial");
+const contUsadas = document.getElementById("acumular-letras");
+const contBoton = document.getElementById("contenedor-boton");
+let palabras = ["carruaje", "mecanico", "programacion", "lenguaje"];
 
-window.onkeyup = (e)=>{
+// btnIngresar.addEventListener("click",(e)=>{
+//   e.preventDefault();
+//   contBoton.classList.toggle("oculto");
+//   contUsadas.classList.remove("oculto");
+//   contenedorCanvas.classList.remove("oculto");
+//   finalizacion.classList.remove("oculto");
+// });
+console.log(contUsadas.value);
+let letrasUsadas = "";
+let palabraEscogida = palabraSecreta(palabras).toUpperCase();
+console.log(palabraEscogida);
+window.onkeyup = (e) => {
   //console.log(String.fromCharCode(e.keyCode));
-}
+  let code = e.keyCode;
+  if (!(code > 64 && code < 91)) return alert("no es una letra");
+  else {
+    let letra = String.fromCharCode(code);
+    if (letrasUsadas.includes(letra)) return alert("Ya usaste esa letra");
+    else if (!palabraEscogida.includes(letra)) {
+      letrasUsadas = `${letrasUsadas} ${letra}`;
+      contUsadas.innerHTML = letrasUsadas;
+    }
+  }
+};
 
-
-const palabraSecreta = (arrayPalabras)=>{
-  let palabra = arrayPalabras[Math.floor(Math.random() * arrayPalabras.length)]
+function palabraSecreta(arrayPalabras) {
+  const ramdon = Math.floor(Math.random() * arrayPalabras.length);
+  let palabra = arrayPalabras[ramdon];
   return palabra;
 }
-
