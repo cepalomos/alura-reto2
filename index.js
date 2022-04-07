bc09da60f90b01f29bcb2e7aa58f80f21ed90652
Carlos Palomo Serna <cepalomos@outlook.es>
2022-04-07 10:40:10 -0500

    Terminado solo falta como cancelar el evento on click

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ const finalizacion = document.getElementById("finalizacion");
 const contenedorCanvas = document.getElementById("tutorial");
 const contUsadas = document.getElementById("acumular-letras");
 const contBoton = document.getElementById("contenedor-boton");
+const lineasPalabra = document.getElementById("palabra");
 let palabras = ["carruaje", "mecanico", "programacion", "lenguaje"];
 let contador = 0;
-
-console.log(contUsadas.value);
+let conteoPalabra = 0;
+let letrasCorrectas = "";
 let letrasUsadas = "";
+
 let palabraEscogida = palabraSecreta(palabras).toUpperCase();
-console.log(palabraEscogida);
+conteoPalabra = palabraEscogida.length;
+generarLineas(conteoPalabra);
+let lista = document.querySelectorAll(".palabra-lista-elemento");
+
 window.onkeyup = (e) => {
   let code = e.keyCode;
   if (!(code > 64 && code < 91)) return alert("no es una letra");
   else {
     let letra = String.fromCharCode(code);
-    if (letrasUsadas.includes(letra)) return alert("Ya usaste esa letra");
+    if (letrasUsadas.includes(letra) || letrasCorrectas.includes(letra))
+      return alert("Ya usaste esa letra");
     else if (!palabraEscogida.includes(letra)) {
       letrasUsadas = `${letrasUsadas} ${letra}`;
       contUsadas.innerHTML = letrasUsadas;
@@ -22,6 +28,17 @@ window.onkeyup = (e) => {
       generadorCanvas(contador);
       if (contador === 7) {
         finalizacion.innerHTML = `Perdiste la palabra era ${palabraEscogida.toLowerCase()}`;
+      }
+    } else {
+      letrasCorrectas = `${letrasCorrectas}${letra}`;
+      for (let i = 0; i < palabraEscogida.length; i++) {
+        if (palabraEscogida[i] === letra) {
+          lista[i].textContent = letra;
+          conteoPalabra--;
+        }
+      }
+      if (!conteoPalabra) {
+        finalizacion.innerHTML = "Ganaste Muchas Felicidades";
       }
     }
   }
@@ -31,4 +48,12 @@ function palabraSecreta(arrayPalabras) {
   const ramdon = Math.floor(Math.random() * arrayPalabras.length);
   let palabra = arrayPalabras[ramdon];
   return palabra;
+}
+function generarLineas(num) {
+  for (let i = 0; i < num; i++) {
+    let li = document.createElement("li");
+    li.classList.add("palabra-lista-elemento");
+    li.textContent = " ";
+    lineasPalabra.appendChild(li);
+  }
 }
